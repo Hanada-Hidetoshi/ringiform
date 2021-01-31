d20a5f7bc6c0dbc836bf25dba21a9ba302b64569
Hanada-Hidetoshi <hideeuph@icloud.dom>
2021-01-31 22:38:12 +0900

    セッションの有無によるログインユーザー名の表示とログイン・ログアウト画面の文言変更

--- header.php
+++ header.php
@@ -1,11 +1,10 @@
+<?php if($_SESSION['user'] !==''){?>
 <header>
   <div>
     <div class="header">
       <div class="top">
         <h1><?php echo $pagetitle ?></h1>
-        <?php if($_SESSION['user'] !== ''){?>
         <p>ログイン中のユーザー：<?php echo $_SESSION['user']; ?></p>
-        <?php } ?>
       </div>
       <div class = "navi">
         <ul>
@@ -19,3 +18,24 @@
     </div>
   </div>
 </header>
+<?php }else{ ?>
+<header>
+  <div>
+    <div class="header">
+      <div class="top">
+        <h1><?php echo $pagetitle ?></h1>
+        <p>ログイン中のユーザー：<?php echo $_SESSION['user']; ?></p>
+      </div>
+      <div class = "navi">
+        <ul>
+          <li><a href="database.php">アカウント情報</a></li>
+          <li><a href="ringidata.php">申請済み稟議データ</a></li>
+          <li><a href="shinsei.php">稟議申請フォーム</a></li>
+          <li><a href="login.php">ログイン</a></li>
+          <li><a href="touroku.php">アカウント登録</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</header>
+<?php } ?>
